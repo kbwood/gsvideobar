8fbc367ff1fc4a1e70295d5ca594a871d3fa3c56
Keith Wood <kbwood@iinet.net.au>
2014-05-24 09:59:28 +1000

    Release 2.0.0

--- jquery.gsvideobar.js
+++ jquery.gsvideobar.js
@@ -1,215 +1,157 @@
 /* http://keith-wood.name/gsvideobar.html
-   Google Search Videobar for jQuery v1.1.0.
+   Google Search Videobar for jQuery v2.0.0.
    See http://www.google.com/uds/solutions/videobar/reference.html.
    Written by Keith Wood (kbwood{at}iinet.com.au) November 2008.
    Available under the MIT (https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt) license. 
    Please attribute the author if you use it. */
+   
+(function($) { // hide the namespace
 
-/* Display a Google Search Videobar.
-   Attach it with options like:
-   $('div selector').gsvideobar({search: ['jquery']});
-*/
+	var pluginName = 'gsvideobar';
 
-(function($) { // Hide scope, no $ conflict
+	/** Create the Google Search Videobar plugin.
+		<p>Sets a <code>div</code> to display a videobar.</p>
+		<p>Expects HTML like:</p>
+		<pre>&lt;div>&lt;/div></pre>
+		<p>Provide inline configuration like:</p>
+		<pre>&lt;div data-gsvideobar="name: 'value'">&lt;/div></pre>
+	 	@module GSVideoBar
+		@augments JQPlugin
+		@example $(selector).gsvideobar({search: ['jquery']}); */
+	$.JQPlugin.createPlugin({
+	
+		/** The name of the plugin. */
+		name: pluginName,
+		
+		/** Cycle times - very short. */
+		cycleVShort: 3000,
+		/** Cycle times - short. */
+		cycleShort: 10000,
+		/** Cycle times - medium (default). */
+		cycleMedium: 15000,
+		/** Cycle times - long. */
+		cycleLong: 30000,
+		/** Cycle modes - random (default). */
+		cycleRandom: 1,
+		/** Cycle modes - linear. */
+		cycleLinear: 2,
+		/** Thumbnail sizes - small. */
+		thumbnailsSmall: 1,
+		/** Thumbnail sizes - medium (default). */
+		thumbnailsMedium: 2,
+		/** YouTube feeds - most viewed. */
+		ytMostViewed: 'ytfeed:most_viewed',
+		/** YouTube feeds - recently featured. */
+		ytRecentlyFeatured: 'ytfeed:recently_featured',
+		/** YouTube feeds - top rated. */
+		ytTopRated: 'ytfeed:top_rated',
+		/** YouTube time modifiers - today. */
+		ytToday: '.today',
+		/** YouTube time modifiers - this week. */
+		ytThisWeek: '.this_week',
+		/** YouTube time modifiers - this month. */
+		ytThisMonth: '.this_month',
+		/** YouTube time modifiers - any time. */
+		ytAllTime: '.all_time',
+			
+		/** Default settings for the plugin.
+			@property [horizontal=true] {boolean} <code>true</code> for horizontal display, <code>false</code> for vertical.
+			@property [thumbnailSize=this.thumbnails] {number} The size of the video thumbnails.
+			@property [player=''] {string|jQuery|Element} jQuery selector, jQuery object,
+						or element for the player area, or '' for a floating player.
+			@property [master=''] {string|jQuery|Element} jQuery selector, jQuery object,
+						or element for the master videobar on the same page.
+			@property [closeText=''] {string} Text displayed at the top of the player to close it.
+			@property [search='jquery'] {string|string[]} Single or list of search terms.
+			@property [manyResults=false] {boolean} <code>true</code> for many results, <code>false</code> for only a few.
+			@property [cycleTime=this.cycleMedium] {number} Time between cycles of the search terms (milliseconds).
+			@property [cycleMode=this.cycleRandom] {number} Mode of cycling through the search terms.
+			@property [statusArea=''] {string|jQuery|Element} jQuery selector, jQuery object,
+						or element for a status area. */
+		defaultOptions: {
+			horizontal: true,
+			thumbnailSize: this.thumbnailsMedium,
+			player: '',
+			master: '',
+			closeText: '',
+			search: 'jquery',
+			manyResults: false,
+			cycleTime: this.cycleMedium,
+			cycleMode: this.cycleRandom,
+			statusArea: ''
+		},
 
-/* GSVideobar manager. */
-function GSVideobar() {
-	this._defaults = {
-		horizontal: true, // True for horizontal display, false for vertical
-		thumbnailSize: this.thumbnailsMedium, // The size of the video thumbnails
-		player: '', // jQuery selector, jQuery object, or element for the player area, or '' for a floating player
-		master: '', // jQuery selector, jQuery object, or element for the master videobar on the same page
-		closeText: '', // Text displayed at the top of the player to close it
-		search: 'jquery', // Single or list of search terms
-		manyResults: false, // True for many results, false for only a few
-		cycleTime: this.cycleMedium, // Time between cycles of the search terms
-		cycleMode: this.cycleRandom, // Mode of cycling through the search terms
-		statusArea: '' // jQuery selector, jQuery object, or element for a status area
-	};
-}
+		/** Specify a YouTube channel to search.
+			@param channel {string} The name of the channel.
+			@return {string} The search parameter. */
+		youTube: function(channel) {
+			return 'ytchannel:' + channel;
+		},
+		
+		_init: function() {
+			this.defaultOptions.thumbnailSize = this.thumbnailsMedium;
+			this.defaultOptions.cycleTime = this.cycleMedium,
+			this.defaultOptions.cycleMode = this.cycleRandom;
+			this._super();
+		},
 
-$.extend(GSVideobar.prototype, {
-	/* Class name added to elements to indicate already configured with GSVideobar. */
-	markerClassName: 'hasGSVideobar',
-	/* Name of the data property for instance settings. */
-	propertyName: 'gsvideobar',
+		_optionsChanged: function(elem, inst, options) {
+			$.extend(inst.options, options);
+			this._updateGSVideobar(elem[0], inst);
+		},
 
-	/* Cycle times. */
-	cycleVShort: 3000,
-	cycleShort: 10000,
-	cycleMedium: 15000, // Default
-	cycleLong: 30000,
-	/* Cycle modes. */
-	cycleRandom: 1, // Default
-	cycleLinear: 2,
-	/* Thumbnail sizes. */
-	thumbnailsSmall: 1,
-	thumbnailsMedium: 2, // Default
-	/* YouTube feeds. */
-	ytMostViewed: 'ytfeed:most_viewed',
-	ytRecentlyFeatured: 'ytfeed:recently_featured',
-	ytTopRated: 'ytfeed:top_rated',
-	/* YouTube time modifiers. */
-	ytToday: '.today',
-	ytThisWeek: '.this_week',
-	ytThisMonth: '.this_month',
-	ytAllTime: '.all_time',
+		/** Redisplay the videobar with an updated display.
+			@private
+			@param elem {Element} The affected division.
+			@param inst {object} The instance settings. */
+		_updateGSVideobar: function(elem, inst) {
+			var getElement = function(selector) {
+				var element = inst.options[selector];
+				element = (element ? (element.jQuery ? element : $(element)) : null);
+				return (element && element.length ? element[0] : null);
+			};
+			var search = inst.options.search;
+			search = ($.isArray(search) ? search : [search]);
+			var player = getElement('player');
+			var master = getElement('master');
+			master = (master ? $.data(master, inst.name).videobar : null);
+			inst.videobar = new GSvideoBar(elem,
+				(master ? null : (player || GSvideoBar.PLAYER_ROOT_FLOATING)),
+				{largeResultSet: inst.options.manyResults, horizontal: inst.options.horizontal,
+				thumbnailSize: inst.options.thumbnailSize, string_allDone: inst.options.closeText,
+				master: master,
+				autoExecuteList: {executeList: search, cycleTime: inst.options.cycleTime,
+					cycleMode: inst.options.cycleMode, statusRoot: getElement('statusArea')}});
+		},
 
-	/* Override the default settings for all GSVideobar instances.
-	   @param  options  (object) the new settings to use as defaults
-	   @return  (GSVideobar) this object */
-	setDefaults: function(options) {
-		$.extend(this._defaults, options || {});
-		return this;
-	},
-
-	/* Specify a YouTube channel to search.
-	   @param  channel  (string) the name of the channel
-	   @return  (string) the search parameter */
-	youTube: function(channel) {
-		return 'ytchannel:' + channel;
-	},
-
-	/* Attach the videobar widget to a div.
-	   @param  target   (element) the control to affect
-	   @param  options  (object) the custom options for this instance */
-	_attachPlugin: function(target, options) {
-		target = $(target);
-		if (target.hasClass(this.markerClassName)) {
-			return;
-		}
-		var inst = {options: $.extend({}, this._defaults, options), target: target};
-		target.addClass(this.markerClassName).data(this.propertyName, inst);
-		this._optionPlugin(target, options);
-	},
-
-	/* Retrieve or reconfigure the settings for a control.
-	   @param  target   (element) the control to affect
-	   @param  options  (object) the new options for this instance or
-	                    (string) an individual property name
-	   @param  value    (any) the individual property value (omit if options
-	                    is an object or to retrieve the value of a setting)
-	   @return  (any) if retrieving a value */
-	_optionPlugin: function(target, options, value) {
-		target = $(target);
-		var inst = target.data(this.propertyName);
-		if (!options || (typeof options == 'string' && value == null)) { // Get option
-			var name = options;
-			options = (inst || {}).options;
-			return (options && name ? options[name] : options);
-		}
-
-		if (!target.hasClass(this.markerClassName)) {
-			return;
-		}
-		options = options || {};
-		if (typeof options == 'string') {
-			var name = options;
-			options = {};
-			options[name] = value;
-		}
-		$.extend(inst.options, options);
-		this._updateGSVideobar(target[0], inst);
-	},
-
-	/* Redisplay the videobar with an updated display.
-	   @param  target  (element) the affected division
-	   @param  inst    (object) the instance settings */
-	_updateGSVideobar: function(target, inst) {
-		var getElement = function(selector) {
-			var element = inst.options[selector];
-			element = (element ? (element.jQuery ? element : $(element)) : null);
-			return (element && element.length ? element[0] : null);
-		};
-		var search = inst.options.search;
-		search = ($.isArray(search) ? search : [search]);
-		var player = getElement('player');
-		var master = getElement('master');
-		master = (master ? $.data(master, this.propertyName).videobar : null);
-		inst.videobar = new GSvideoBar(target,
-			(master ? null : (player || GSvideoBar.PLAYER_ROOT_FLOATING)),
-			{largeResultSet: inst.options.manyResults, horizontal: inst.options.horizontal,
-			thumbnailSize: inst.options.thumbnailSize, string_allDone: inst.options.closeText,
-			master: master,
-			autoExecuteList: {executeList: search, cycleTime: inst.options.cycleTime,
-				cycleMode: inst.options.cycleMode, statusRoot: getElement('statusArea')}});
-	},
-
-	/* Perform a new seacrh in the videobar.
-	   @param  target  (element) the affected division
-	   @param  search  (string) the new search terms */
-	_searchPlugin: function(target, search) {
-		var inst = $.data(target, this.propertyName);
-		if (inst) {
-			$.extend(inst.options, {search: search});
-			inst.videobar.execute(search);
-		}
-	},
-
-	/* Remove the plugin functionality from a control.
-	   @param  target  (element) the control to affect */
-	_destroyPlugin: function(target) {
-		target = $(target);
-		if (!target.hasClass(this.markerClassName)) {
-			return;
-		}
-		target.removeClass(this.markerClassName).empty().removeData(this.propertyName);
-	}
-});
-
-// The list of commands that return values and don't permit chaining
-var getters = [];
-
-/* Determine whether a command is a getter and doesn't permit chaining.
-   @param  command    (string, optional) the command to run
-   @param  otherArgs  ([], optional) any other arguments for the command
-   @return  true if the command is a getter, false if not */
-function isNotChained(command, otherArgs) {
-	if (command == 'option' && (otherArgs.length == 0 ||
-			(otherArgs.length == 1 && typeof otherArgs[0] == 'string'))) {
-		return true;
-	}
-	return $.inArray(command, getters) > -1;
-}
-
-/* Attach the GSVideobar functionality to a jQuery selection.
-   @param  options  (object) the new settings to use for these instances (optional) or
-                    (string) the command to run (optional)
-   @return  (jQuery) for chaining further calls or
-            (any) getter value */
-$.fn.gsvideobar = function(options) {
-	var otherArgs = Array.prototype.slice.call(arguments, 1);
-	if (isNotChained(options, otherArgs)) {
-		return plugin['_' + options + 'Plugin'].apply(plugin, [this[0]].concat(otherArgs));
-	}
-	return this.each(function() {
-		if (typeof options == 'string') {
-			if (!plugin['_' + options + 'Plugin']) {
-				throw 'Unknown command: ' + options;
+		/** Perform a new search in the videobar.
+			@param elem {Element} The affected division.
+			@param search {string} The new search terms. */
+		search: function(elem, search) {
+			var inst = this._getInst(elem);
+			if (inst) {
+				$.extend(inst.options, {search: search});
+				inst.videobar.execute(search);
 			}
-			plugin['_' + options + 'Plugin'].apply(plugin, [this].concat(otherArgs));
-		}
-		else {
-			plugin._attachPlugin(this, options || {});
+		},
+
+		_preDestroy: function(elem, inst) {
+			elem.empty();
 		}
 	});
-};
 
-// Add required external files - note: key must be set before loading this module
-if ($('script[src*="www.google.com/uds/api?file=uds.js"]').length == 0) {
-	if (!$.googleSearchKey) {
-		throw 'Missing Google Search Key';
+	// Add required external files - note: key must be set before loading this module
+	if ($('script[src*="www.google.com/uds/api?file=uds.js"]').length === 0) {
+		if (!$.googleSearchKey) {
+			throw 'Missing Google Search Key';
+		}
+		document.write('<script type="text/javascript" src="http://www.google.com/uds/' +
+			'api?file=uds.js&v=1.0&key=' + $.googleSearchKey + '"></script>\n' +
+			'<link type="text/css" href="http://www.google.com/uds/css/gsearch.css" rel="stylesheet"/>\n');
 	}
 	document.write('<script type="text/javascript" src="http://www.google.com/uds/' +
-		'api?file=uds.js&v=1.0&key=' + $.googleSearchKey + '"></script>\n' +
-		'<link type="text/css" href="http://www.google.com/uds/css/gsearch.css" rel="stylesheet"/>\n');
-}
-document.write('<script type="text/javascript" src="http://www.google.com/uds/' +
-	'solutions/videobar/gsvideobar.js"></script>\n' +
-	'<link type="text/css" href="http://www.google.com/uds/solutions/videobar/gsvideobar.css" ' +
-	'rel="stylesheet"/>\n');
-
-/* Initialise the GSVideobar functionality. */
-var plugin = $.gsvideobar = new GSVideobar(); // Singleton instance
+		'solutions/videobar/gsvideobar.js"></script>\n' +
+		'<link type="text/css" href="http://www.google.com/uds/solutions/videobar/gsvideobar.css" ' +
+		'rel="stylesheet"/>\n');
 
 })(jQuery);
